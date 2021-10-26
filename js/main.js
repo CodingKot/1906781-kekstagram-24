5f3378bf043cf99cc16706248253befb2a805cf2
CodingKot <blackcat.89@inbox.ru>
2021-10-26 16:04:47 +0300

    На странице отрисованы картинки

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-import {DESCRIPTIONS_NUMBER, createPictureDescription} from '/utils/data-creator.js';
-
-const PictureDescriptions = Array.from({
-  length: DESCRIPTIONS_NUMBER,
-}, createPictureDescription);
-
-for (let i = 0; i <= DESCRIPTIONS_NUMBER; i++) {
-  let property = '';
-  let txt = '';
-  const description = PictureDescriptions[i];
-  for (property in description) {
-    txt += `${description[property]} `;
-  }
-  const testData  = document.createElement('p');
-  testData.textContent = txt;
-  const parent = document.querySelector('body');
-  parent.append(testData);
-}
+import './utils/data-creator.js';
+import './utils/draw-pictures.js';
